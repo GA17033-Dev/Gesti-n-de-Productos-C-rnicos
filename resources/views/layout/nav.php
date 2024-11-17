3bb4deee0980b4a9af39bcbdc9d180d38d1bf599
alexis-garcia-dev <alexis.garcia170698@gmail.com>
2024-11-16 18:48:47 -0600

    Remove commented-out search form and dropdown from navigation layout

--- resources/views/layout/nav.php
+++ resources/views/layout/nav.php
@@ -1,17 +1,17 @@
 <nav class="navbar navbar-expand bg-white shadow mb-4 topbar">
     <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle me-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
-        <form class="d-none d-sm-inline-block me-auto ms-md-3 my-2 my-md-0 mw-100 navbar-search">
+        <!-- <form class="d-none d-sm-inline-block me-auto ms-md-3 my-2 my-md-0 mw-100 navbar-search">
             <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Buscar..." aria-label="Search" aria-describedby="basic-addon2"><button class="btn btn-primary py-0" type="button"><i class="fas fa-search"></i></button></div>
-        </form>
+        </form> -->
         <ul class="navbar-nav flex-nowrap ms-auto">
-            <li class="nav-item dropdown d-sm-none no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><i class="fas fa-search"></i></a>
+            <!-- <li class="nav-item dropdown d-sm-none no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><i class="fas fa-search"></i></a>
                 <div class="dropdown-menu dropdown-menu-end p-3 animated--grow-in" aria-labelledby="searchDropdown">
                     <form class="me-auto navbar-search w-100">
                         <div class="input-group"><input class="bg-light border-0 form-control small" type="text" placeholder="Buscar..." aria-label="Search" aria-describedby="basic-addon2">
                             <div class="input-group-append"><button class="btn btn-primary py-0" type="button"><i class="fas fa-search"></i></button></div>
                     </form>
                 </div>
-            </li>
+            </li> -->
             <li class="nav-item dropdown no-arrow mx-1">
                 <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#"><span class="badge bg-danger badge-counter">3+</span><i class="fas fa-bell fa-fw"></i></a>
                     <div class="dropdown-menu dropdown-menu-end dropdown-list animated--grow-in">
